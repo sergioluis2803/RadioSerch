a3f4a596912cead64ddc46f825a2ba17ed012f6e
sergioluis2803 <sergiomv2803@gmail.com>
2023-08-04 22:37:45 -0500

    create new rama

--- app/src/main/java/com/example/radioserch/features/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/radioserch/features/home/presentation/HomeScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import com.example.radioserch.features.navigation.Screen
@@ -27,7 +28,8 @@ fun Home(paddingValues: PaddingValues, navController: NavHostController) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(paddingValues)
+            .padding(paddingValues),
+        contentAlignment = Alignment.Center
     ) {
         Button(onClick = {
             FirebaseAuth.getInstance().signOut()
